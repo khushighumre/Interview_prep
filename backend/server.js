8d223399626dd18ac24b0ae80c1895e52a69a980
Khushi Ghumre <khushighumre@gmail.com>
2025-08-03 18:41:24 +0530

    done with today - models

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const path = require("path");
+const connectDB = require("./config/db");
 
 const app = express();
 
@@ -14,10 +15,18 @@ app.use(
     })
 );
 
+connectDB()
+
 // Middleware
 app.use(express.json());
 
 // routes
+app.use("/api/auth", authRoutes);
+app.use("/api/sessions", sessionRoutes);
+app.use("/api/questions", questionRoutes);
+
+app.use("/api/ai/generate-questions", protect, generateInterviewQuestions);
+app.use("/api/ai/generate-explaination", protect, generateConceptExplaination);
 
 // Serve uploads folder
 app.use('/uploads', express.static(path.join(__dirname,"uploads"), {}));
